a7e0d98ff397b944fbe99ab595f5da1ccae638cc
pushpak261 <pushpakrajendra@gmail.com>
2024-10-30 10:41:08 +0530

    commit

---
+++ src/main/java/com/ecommerce/sportshub/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.ecommerce.sportshub.security;
+
+import com.ecommerce.sportshub.entity.User;
+import com.ecommerce.sportshub.exception.NotFoundException;
+import com.ecommerce.sportshub.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User/ Email Not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
