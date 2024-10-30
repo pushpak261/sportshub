f3fc3844c13c260647bcef66d7a9661b85437cf2
pushpak261 <pushpakrajendra@gmail.com>
2024-10-30 07:05:14 +0530

    commit

---
+++ src/main/java/com/ecommerce/sportshub/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.ecommerce.sportshub.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message){
+        super(message);
+    }
+}
